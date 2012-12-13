d25be9b0ed5ea8000cc4b6cc65feece03e463251
Jason Paul Becker <jason@Jason-Beckers-MacBook-Air.local>
2012-12-13 12:26:37 -0500

    Changed working directory to reflect new location due to git version control and added the executedAt object to keep track of when the script is sourced.

--- buildTables.R
+++ buildTables.R
@@ -1,11 +1,12 @@
 # Build individual year tables.
 
-setwd('/Users/jason/Google Drive/ProvidenceData/')
+setwd('/Users/jason/R Projects/PPSDColllegeReadiness/')
 
 source('dependencies.R')
 source('functions.R')
 source('load.R')
 
+executedAt <- paste(Sys.time())
 tables2004_2005 <- build_tables(reg0405, 2005)
 tables2005_2006 <- build_tables(reg0506, 2006)
 tables2006_2007 <- build_tables(reg0607, 2007)
