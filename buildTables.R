8d2cb98ec66342c3f8317464f13a37350f836d26
jasonpbecker <jason.paul.becker@gmail.com>
2012-12-29 01:10:40 -0500

    Added all the years.

--- buildTables.R
+++ buildTables.R
@@ -10,3 +10,8 @@ executedAt <- paste(Sys.time())
 tables2004_2005 <- build_tables(reg0405, 2005)
 tables2005_2006 <- build_tables(reg0506, 2006)
 tables2006_2007 <- build_tables(reg0607, 2007)
+tables2007_2008 <- build_tables(reg0708, 2008)
+tables2008_2009 <- build_tables(reg0809, 2009)
+tables2009_2010 <- build_tables(reg0910, 2010)
+tables2010_2011 <- build_tables(reg1011, 2011)
+
