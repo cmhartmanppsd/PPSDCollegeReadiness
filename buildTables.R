ad3590f762f4f1c72aadff2c78f1ae0753627a2d
jasonpbecker <jason.paul.becker@gmail.com>
2013-01-29 14:35:47 -0500

    Add workflow for correct calculation of race and sex for person table.

--- buildTables.R
+++ buildTables.R
@@ -15,3 +15,19 @@ tables2008_2009 <- build_tables(reg0809, 2009)
 tables2009_2010 <- build_tables(reg0910, 2010)
 tables2010_2011 <- build_tables(reg1011, 2011)
 
+early_race <- modal_person_attribute(rbind(tables2005_2006$person, 
+                                           tables2006_2007$person, 
+                                           tables2007_2008$person,
+                                           tables2008_2009$person, 
+                                           tables2009_2010$person), 'race')
+late_race <- modal_person_attribute(tables2010_2011$person, 'race')
+early_race$schoolyear <- '2009_2010'
+late_race$schoolyear <- '2010_2011'
+race <- modal_person_attribute(rbind(early_race, late_race), 'race')
+
+sex <- modal_person_attribute(rbind(tables2005_2006$person, 
+                                    tables2006_2007$person, 
+                                    tables2007_2008$person,
+                                    tables2008_2009$person, 
+                                    tables2009_2010$person,
+                                    tables2010_2011$person), 'sex')
