e370cb87cca45b306775d25ebf1113525ead98e1
jasonpbecker <jason.paul.becker@gmail.com>
2013-01-29 14:51:22 -0500

    Add graduation flag calculations for person table.

--- buildTables.R
+++ buildTables.R
@@ -14,7 +14,9 @@ tables2007_2008 <- build_tables(reg0708, 2008)
 tables2008_2009 <- build_tables(reg0809, 2009)
 tables2009_2010 <- build_tables(reg0910, 2010)
 tables2010_2011 <- build_tables(reg1011, 2011)
+###
 
+# Build cannonical race for person table
 early_race <- modal_person_attribute(rbind(tables2005_2006$person, 
                                            tables2006_2007$person, 
                                            tables2007_2008$person,
@@ -25,9 +27,21 @@ early_race$schoolyear <- '2009_2010'
 late_race$schoolyear <- '2010_2011'
 race <- modal_person_attribute(rbind(early_race, late_race), 'race')
 
+# Build cannonical sex for person table
 sex <- modal_person_attribute(rbind(tables2005_2006$person, 
                                     tables2006_2007$person, 
                                     tables2007_2008$person,
                                     tables2008_2009$person, 
                                     tables2009_2010$person,
                                     tables2010_2011$person), 'sex')
+
+# Build cannonical graduation flag and date for person table
+graduated <- rbind(tables2005_2006$enroll, tables2006_2007$enroll, 
+                   tables2007_2008$enroll, tables2008_2009$enroll,
+                   tables2009_2010$enroll, tables2010_2011$enroll)
+first9th <- aggregate(schoolyear~sasid, data=subset(graduated, grade==9), min)
+names(first9th)[2] <- 'first9th'
+graduated$graduated <- with(graduated, ifelse(exit_type==15, 'Y','N'))
+graduated$grad_date <- with(graduated, ifelse(graduated=='Y', exit_date, NA))
+graduated$grad_date <- as.Date(graduated$grad_date, origin='1970-01-01')
+graduated <- graduated[,c('sasid', 'graduated', 'grad_date')]
