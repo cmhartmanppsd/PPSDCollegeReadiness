636cab7345411b0af1aa9c8cff88d2423a0fa908
jasonpbecker <jason.paul.becker@gmail.com>
2013-01-30 13:08:53 -0500

    Edited the modal_person_attribute to speed it up by using data.table to address issue #3. The speed up is about 4.5x. I will want to revisit this function again to hopefully achieve 10x or better.

--- functions.R
+++ functions.R
@@ -247,39 +247,25 @@ modal_person_attribute <- function(df, attribute){
   # df: rbind of all person tables from all years
   # attribute: vector name to calculate the modal value
   # Calculate the number of instances an attributed is associated with an id
-  mode <- do.call(rbind, 
-                  tapply(as.character(df[[attribute]]), df$sasid, 
-                         function(x) data.frame(attribute=rle(x)$values, 
-                                                counts=rle(x)$lengths)))
-  names(mode) <- c(as.character(attribute), 'counts')
-  # Clean up
-  mode$sasid <- row.names(mode)
-  row.names(mode) <- NULL
-  # Remove the R .1, .2 notation for repeat SASIDs from tapply
-  mode$sasid <- gsub('[.][0-9]$','', mode$sasid)
-  # Only include attributes with the maximum values. This is equivalen to the
+  dt <- data.table(df)
+  mode <- dt[, rle(as.character(.SD[[attribute]])), by=sasid]
+  setnames(mode, c('sasid', 'counts', as.character(attribute)))
+  setkeyv(mode, c('sasid', 'counts'))
+  # Only include attributes with the maximum values. This is equivalent to the
   # mode with two records when there is a tie.
-  mode <- ddply(mode, 'sasid', function(mode) subset(mode, 
-                                                     counts==max(counts)))
-  # Subset of all cases where there is a duplicate mode for the attribute
-  dupes <- subset(mode, sasid %in% subset(mode$sasid, duplicated(sasid)))
-  # Create a non-duplicated modal value subset
-  mode <- subset(mode, sasid %in% (sasid %w/o% dupes$sasid))
+  mode <- mode[,subset(.SD, counts==max(counts)), by=sasid]
+  mode[,counts:=NULL]
+  setnames(mode, c('sasid', attribute))
+  setkeyv(mode, c('sasid',attribute))
   # Produce the maximum year value associated with each ID-attribute pairing    
-  max_year <- aggregate(schoolyear~ sasid + df[[attribute]], data=df, max)
-  names(max_year)[2] <- as.character(attribute)
-  # Resolve the duplicates. This merges in the year value for each of the
-  # modal attributes, arranges the result by ID and descending school year, 
-  # and remove the duplicates.
-  resolved <- subset(arrange(merge(dupes, max_year, 
-                                   all.x=TRUE, all.y=FALSE), 
-                             sasid, desc(schoolyear)), 
-                     !duplicated(sasid))
-  # Create the result dataframe that contains ID and the corrected attribute
-  # values.
-  resolved <- resolved[, c('sasid', attribute)]
-  mode <- mode[, c('sasid', attribute)]
-  result <- rbind(mode, resolved)
+  setkeyv(dt, c('sasid',attribute))
+  mode <- dt[,list(schoolyear=max(schoolyear)), by=c("sasid", attribute)][mode]
+  setkeyv(mode, c('sasid', 'schoolyear'))
+  # Select the last observation for each ID, which is equivalent to the highest
+  # schoolyear value associated with the most frequent attribute.
+  result <- mode[,lapply(.SD, tail, 1), by=sasid]
+  # Remove the schoolyear to clean up the result
+  result <- result[,schoolyear:=NULL]
   return(result)
 }
 
