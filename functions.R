383c292ee8ed42a9db294aa11aa0e8f7a57c30b1
jasonpbecker <jason.paul.becker@gmail.com>
2013-02-05 00:22:49 -0500

    Added "select_hs" which is a function to get the first, last, and longest attended high school. Could still use some speed up-- takes about 25 seconds per run.

--- functions.R
+++ functions.R
@@ -247,7 +247,7 @@ modal_person_attribute <- function(df, attribute){
   # df: rbind of all person tables from all years
   # attribute: vector name to calculate the modal value
   # Calculate the number of instances an attributed is associated with an id
-  dt <- data.table(df)
+  dt <- data.table(df, key='sasid')
   mode <- dt[, rle(as.character(.SD[[attribute]])), by=sasid]
   setnames(mode, c('sasid', 'counts', as.character(attribute)))
   setkeyv(mode, c('sasid', 'counts'))
@@ -268,7 +268,52 @@ modal_person_attribute <- function(df, attribute){
   result <- result[,schoolyear:=NULL]
   return(result)
 }
+# potential speedup
+modal_person_attribute2 <- function(df, attribute){
+  # df: rbind of all person tables from all years
+  # attribute: vector name to calculate the modal value
+  # Calculate the number of instances an attributed is associated with an id
+  dt <- data.table(df, key='sasid')
+  mode <- dt[, rle(as.character(get(attribute))), by=sasid]
+  setnames(mode, c('sasid', 'counts', as.character(attribute)))
+  setkeyv(mode, c('sasid',attribute))
+  mode <- dt[,list(schoolyear=max(schoolyear)), by=c("sasid", attribute)][mode]
+  setkeyv(mode, c('sasid', 'schoolyear'))
+  # Select the last observation for each ID, which is equivalent to the highest
+  # schoolyear value associated with the most frequent attribute.
+  result <- mode[,.SD[.N,],by=sasid]
+  # Remove the schoolyear to clean up the result
+  result <- result[,schoolyear:=NULL]
+  return(result)
+}
 
+select_hs <- function(df, type="first"){
+  dt <- data.table(df, key=c('sasid'))
+  if(type=="first"){
+    hs <- dt[grade %in% c(9,10,11,12), .SD[which.min(enroll_date)], 
+             by=key(dt)][]
+#     test <- dt[grade %in% c(9,10,11,12), list(enroll_date=min(enroll_date, na.rm=TRUE)), by=key(dt)] 
+#     setkeyv(test, c('sasid','enroll_date'))
+#     setkeyv(dt, c('sasid', 'enroll_date'))
+#     test <- test[dt]
+  }
+  else if(type=="last"){
+    hs <- dt[grade %in% c(9,10,11,12), .SD[which.max(exit_date)], 
+             by=key(dt)]
+  }
+  else if(type=="long"){
+    hs <- dt[grade %in% c(9,10,11,12), list(total_adm = sum(adm)), 
+             by=c(key(dt),'schno')]
+    setkeyv(hs, c('sasid', 'schno'))
+    setkeyv(dt, c('sasid', 'schno'))
+    hs<- hs[dt[grade %in% c(9, 10, 11, 12)]][,.SD[which.max(total_adm)], 
+                                             by=sasid]
+  }
+  else{
+    stop(paste("Unrecognized type=", type))
+  }
+  return(hs[,list(sasid, schno, school, schoolyear, grade, exit_type)])
+}
 
 build_tables <- function(regYr, year){
   if(exists('executedAt')==TRUE){
