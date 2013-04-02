c5f2367089cd6a8988ae92c6ec50382f00b8c8a7
jasonpbecker <jason.paul.becker@gmail.com>
2013-04-02 10:17:29 -0400

    Line 10 commented out

--- buildTables.R
+++ buildTables.R
@@ -7,7 +7,7 @@ source('functions.R')
 source('load.R')
 
 executedAt <- paste(Sys.time())
-tables2004_2005 <- build_tables(reg0405, 2005)
+# tables2004_2005 <- build_tables(reg0405, 2005)
 tables2005_2006 <- build_tables(reg0506, 2006)
 tables2006_2007 <- build_tables(reg0607, 2007)
 tables2007_2008 <- build_tables(reg0708, 2008)
