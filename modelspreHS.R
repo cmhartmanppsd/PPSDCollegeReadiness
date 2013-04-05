004f128d1acf8b16f6cdc01d4da46f80d8232507
jasonpbecker <jason.paul.becker@gmail.com>
2013-04-05 13:09:32 -0400

    Fix spelling, add graphics
    Fixed spelling on line 10 and added more information to plot of
    incoming risk by high school.

--- modelspreHS.R
+++ modelspreHS.R
@@ -7,7 +7,7 @@ basemodel8thgrade <- glm(as.numeric(as.factor(graduated))-1 ~ sex +
                          data=hscohort0708, family=binomial(link='logit'))
 hs0708$predict8th<- predict(basemodel8thgrade, newdata=hs0708, type='response')
 
-repeat9th.glm <- glm(as.numeric(as.factor(willrepeatgr))-1 ~ sex + attend8th
+repeat9th.glm <- glm(as.numeric(as.factor(willrepeatgr))-1 ~ sex + attendance8th
                      + gpa8th + reanormal + ageHS + I(schno_first=='164'),
                      data=hscohort0708, family=binomial(link='logit'))
 
@@ -22,4 +22,15 @@ text(.6, .6, 'Proposed Cutoff')
 ggplot(data=hs0708, aes(school_first, predict8th)) + 
 geom_boxplot() + 
 scale_y_continuous(labels=percent_format()) +
+theme(axis.text.x = element_text(angle = 60, hjust = 1))
+
+incomingrisk <- subset(data.frame(prop.table(table(hs0708$school_first, 
+                                                   hs0708$predict8th<.45), 1)), 
+                       Var2==TRUE)
+incomingrisk$Var2 <- NULL
+names(incomingrisk) <- c('School', 'HighRisk')
+incomingrisk <- subset(incomingrisk, !is.na(HighRisk))
+
+ggplot(data=incomingrisk, aes(School, HighRisk)) + 
+geom_bar() + scale_y_continuous(labels=percent_format()) +
 theme(axis.text.x = element_text(angle = 60, hjust = 1))
