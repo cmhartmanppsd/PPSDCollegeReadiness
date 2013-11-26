4b3642d46e802fa7fe534d23a10b61e5e59adde4
jasonpbecker <jason.paul.becker@gmail.com>
2013-11-26 14:33:05 -0500

    Data past 2011
    This is the beginning of a new function to build tables out of data
    sources that are not coming from a pre-combined master data file.

--- R/build_tables.R
+++ R/build_tables.R
@@ -46,3 +46,41 @@ build_tables <- function(regYr, year){
                  course=tbl_course, missingSASID=noSASID, dupes=duplicates)
   return(tables)
 }
+
+build_tables_later <- function(stu, enr, gra, year){
+  if(exists('executedAt')==TRUE){
+    logAt <- paste('logs/', executedAt, 'build.log', sep='')
+  }else{
+    logAt <- paste('logs/', paste(Sys.time()), 'build.log', sep='')
+  }
+  noSASID <- missingSASID(stu)
+  buildLog <- capture.output(head(noSASID))
+  cat('noSASID', year, buildLog, '\n\n', file=logAt, sep='\n', 
+      append=TRUE)
+  duplicates <- duplicateSASID(stu)
+  buildLog <- capture.output(head(duplicates))
+  cat('duplicates', year, buildLog, '\n\n', file=logAt, sep='\n', 
+      append=TRUE)
+  tbl_person <- extract_person(stu)
+  buildLog <- capture.output(head(tbl_person)[1:10])
+  cat('tbl_person', year, buildLog, '\n\n', file=logAt, sep='\n',
+      append=TRUE)
+  tbl_person_annual <- extract_person_annual(stu)
+  buildLog <- capture.output(head(tbl_person_annual)[1:10])
+  cat('tbl_person_annual', year, buildLog, '\n\n', file=logAt, sep='\n', 
+      append=TRUE)
+  tbl_enrollment <- enr
+  buildLog <- capture.output(head(tbl_enrollment)[1:10])
+  cat('tbl_enrollment', buildLog, year, '\n\n', file=logAt, sep='\n', 
+      append=TRUE)
+  # Achievement
+
+  tbl_course <- extract_course(gra)
+  buildLog <- capture.output(head(tbl_course)[1:10])
+  cat('tbl_course', buildLog, year, '\n\n', file=logAt, sep='\n', 
+      append=TRUE)
+  tables <- list(person=tbl_person, person_annual=tbl_person_annual, 
+                 enrollment=tbl_enrollment, achievement=tbl_achievement,
+                 course=tbl_course, missingSASID=noSASID, dupes=duplicates)
+  
+}
