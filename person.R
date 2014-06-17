4ccd48ec5bffc42da1462e7b8533033570c19dd4
jasonpbecker <jason.paul.becker@gmail.com>
2014-06-17 11:25:12 -0400

    Added missing year to person_annual. Whoops.

--- person.R
+++ person.R
@@ -81,6 +81,7 @@ annual_demos <- rbind(tables2005_2006$person_annual,
                       tables2007_2008$person_annual,
                       tables2008_2009$person_annual, 
                       tables2009_2010$person_annual,
+                      tables2010_2011$person_annual,
                       tables2011_2012$person_annual,
                       tables2012_2013$person_annual)
 
